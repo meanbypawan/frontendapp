4381d93b60442087bc81cd22cc8b730b2f44bf3a
mean <meanstack4@gmail.com>
2025-06-24 17:07:51 +0530

    addint domain

--- src/apis.js
+++ src/apis.js
@@ -1,8 +1,8 @@
 export default {
-    SIGN_UP: "/user/sign-up",
-    SIGN_IN:"/user/sign-in",
-    PRODUCT_LIST:"/product/list",
-    PRODUCT_BY_ID:"/product",
-    PLACE_ORDER:"/order/place-order",
-    ORDER_LIST:"/order/list"
+    SIGN_UP: "https://backendapp-dx23.onrender.com/user/sign-up",
+    SIGN_IN:"https://backendapp-dx23.onrender.com/user/sign-in",
+    PRODUCT_LIST:"https://backendapp-dx23.onrender.com/product/list",
+    PRODUCT_BY_ID:"https://backendapp-dx23.onrender.com/product",
+    PLACE_ORDER:"https://backendapp-dx23.onrender.com/order/place-order",
+    ORDER_LIST:"https://backendapp-dx23.onrender.com/order/list"
 }
